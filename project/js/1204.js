e8d4fc2332deadb8f019faff7599ac51b9b63cbb
nekosyakolya <greirex@mail.ru>
2016-06-21 09:31:30 +0300

    изменение функции

--- project/js/1204.js
+++ project/js/1204.js
@@ -14,39 +14,41 @@ function img()
     i++;
 };
 
-
-
 var menu_selector = ".menu_block_header"; 
  
-function onScroll(){
+function onScroll()
+{
     var scroll_top = $(document).scrollTop();
-    $(menu_selector + " a").each(function(){
+    $(menu_selector + " a").each(function()
+    {
         var hash = $(this).attr("href");
         var target = $(hash);
-        if (target.position().top <= scroll_top && target.position().top + target.outerHeight()> scroll_top) {
+        if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top)
+        {
             $(menu_selector + " a.active").removeClass("active");
             $(this).addClass("active");
-        } else {
+        } 
+        else
+        {
             $(this).removeClass("active");
         }
     });
 }
-$(document).ready(function () {
- 
+$(document).ready(function ()
+{
     $(document).on("scroll", onScroll);
- 
-    $("a[href^=#]").click(function(e){
+    $("a[href^=#]").click(function(e)
+    {
         e.preventDefault();
- 
         $(document).off("scroll");
         $(menu_selector + " a.active").removeClass("active");
         $(this).addClass("active");
         var hash = $(this).attr("href");
         var target = $(hash);
- 
         $("html, body").animate({
             scrollTop: target.offset().top
-        }, 500, function(){
+        }, 500, function()
+        {
             window.location.hash = hash;
             $(document).on("scroll", onScroll);
         });
