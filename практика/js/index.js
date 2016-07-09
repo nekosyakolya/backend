ae585498714cd0ad51bd549541f43da79b0e9109
nekosyakolya <greirex@mail.ru>
2016-07-10 01:22:36 +0300

    практика.попиксельность.начало!
    попиксельность блоков.оптимизация css
    изменения классов

--- практика/js/index.js
+++ практика/js/index.js
@@ -1,12 +1,12 @@
-var button = $(".phone_contact_text");
+var button = $(".contact_text");
 var close = $(".close");
 button.click(function()
 {
     close.css("display", "inline-block");
     $(".support_block").addClass("white");
     $(".support_block").addClass("allocation");
-    $(".secret_block").css("display", "block");
-    $(".secret_block").addClass("allocation");    
+    $(".open_block").css("display", "block");
+    $(".open_block").addClass("allocation");    
 });
 
 close.click(function()
@@ -14,13 +14,5 @@ close.click(function()
     close.css("display", "none");
     $(".support_block").removeClass("allocation"); 
     $(".support_block").removeClass("white"); 
-    $(".secret_block").css("display", "none");
-});
-
-$(".phone_contact").click(function()
-{
-    close.css("display", "none");
-    $(".support_block").removeClass("allocation"); 
-    $(".support_block").removeClass("white"); 
-    $(".secret_block").css("display", "none");
+    $(".open_block").css("display", "none");
 });
